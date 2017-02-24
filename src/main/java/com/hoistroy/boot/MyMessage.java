c5e40a873d0ce2835f1ce8a96333ed84c991181f
SupachaiChaipratum <ch.supachai.aon@gmail.com>
2017-02-25 01:09:15 +0700

    Initial commit

---
+++ src/main/java/com/hoistroy/boot/MyMessage.java
@@ -0,0 +1,23 @@
+package com.hoistroy.boot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyMessage {
+
+    @Value("${my.messageValue}")
+    private int messageValue;
+
+
+    public int getMessageValue() {
+        return messageValue;
+    }
+
+    public void setMessageValue(int messageValue) {
+        this.messageValue = messageValue;
+    }
+
+
+
+}
